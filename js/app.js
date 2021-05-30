0814bf7ed7b4955a74e9527075eae113befb5569
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-05-30 16:23:03 +0300

    finished the first object

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
-
-let salesArr = [];
+let timeArr = ['6am :','7am :','8am :','9am :','10am :','11am :','12pm :','1pm :','2pm :','3pm :','4pm :','5pm :','6pm :','7pm :', 'Total :']
+let golbeVar = document.getElementById('main');
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -23,7 +23,7 @@ let Seattle ={
   cookiesAmount : function(){
     for (let i=0 ; i < 14 ; i++ ) {
       let cookieAmountPerH = this.randCusHor() * this.avgCokie;
-      this.cookieSoldPerHour.push(cookieAmountPerH);
+      this.cookieSoldPerHour.push(Math.floor(cookieAmountPerH));
       console.log(this.cookieSoldPerHour);
     }
 
@@ -33,16 +33,27 @@ let Seattle ={
     for(let i=0 ; i < this.cookieSoldPerHour.length ; i++){
       this.totalCokiesSold = this.totalCokiesSold + this.cookieSoldPerHour[i];
     }
+    this.cookieSoldPerHour.push(this.totalCokiesSold);
 
   },
   renderCookieAmmount : function(){
-    
+
+    let ulEl = golbeVar.appendChild(document.createElement('ul'));
+    for ( let i=0 ; i<timeArr.length ; i++) {
+
+      let liEl = document.createElement('li');
+      ulEl.appendChild(liEl);
+      liEl.textContent =`${timeArr[i]} ${this.cookieSoldPerHour[i]}`;
+    }
+
+
   },
 
 
 }
 
 Seattle.cookiesAmount();
-Seattle.totalCokieSoldFunc()
+Seattle.totalCokieSoldFunc();
+Seattle.renderCookieAmmount();
 console.log(Seattle);
 console.log(Seattle.totalCokiesSold);
