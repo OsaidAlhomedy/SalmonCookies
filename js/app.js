d53abe766be87d41bb87cbcc3788f515fdfcda65
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-05-31 15:09:02 +0300

    added most of the requiremnts

--- js/app.js
+++ js/app.js
@@ -1,287 +1,110 @@
 'use strict';
-let timeArr = ['6am :','7am :','8am :','9am :','10am :','11am :','12pm :','1pm :','2pm :','3pm :','4pm :','5pm :','6pm :','7pm :', 'Total :'];
+let timeArr = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','Daily Location Total'];
 let golbeVar = document.getElementById('main');
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-// This is Seattle Object
+let tableEl = document.createElement('table');
+golbeVar.appendChild(tableEl);
+let arrOfObjects = [];
 
-let Seattle ={
+function Shops(shoploc,minCust,maxCust,avgCokie){
 
-  shopLoc : 'Seattle',
-  minCust : 23 ,
-  maxCust : 65 ,
-  avgCokie : 6.3 ,
-  cookieSoldPerHour :[],
-  totalCokiesSold : null,
-
-  randCusHor : function(){
-    let randCustomers = getRndInteger(this.minCust,this.maxCust)
-    return randCustomers;
-  },
-  cookiesAmount : function(){
-    for (let i=0 ; i < 14 ; i++ ) {
-      let cookieAmountPerH = this.randCusHor() * this.avgCokie;
-      this.cookieSoldPerHour.push(Math.floor(cookieAmountPerH));
-      console.log(this.cookieSoldPerHour);
-    }
-
-  },
-
-  totalCokieSoldFunc : function() {
-    for(let i=0 ; i < this.cookieSoldPerHour.length ; i++){
-      this.totalCokiesSold = this.totalCokiesSold + this.cookieSoldPerHour[i];
-    }
-    this.cookieSoldPerHour.push(this.totalCokiesSold);
-
-  },
-  renderCookieAmmount : function(){
-
-    let h2El = golbeVar.appendChild(document.createElement('h2'));
-    h2El.textContent = this.shopLoc;
-
-    let ulEl = golbeVar.appendChild(document.createElement('ul'));
-    for ( let i=0 ; i<timeArr.length ; i++) {
-
-      let liEl = document.createElement('li');
-      ulEl.appendChild(liEl);
-      liEl.textContent =`${timeArr[i]} ${this.cookieSoldPerHour[i]}`;
-    }
-
-
-  },
-
+  this.shopLoc = shoploc;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCokie = avgCokie;
+  this.cookieSoldPerHour =[];
+  this.totalCokiesSold = null;
 
 }
 
-Seattle.cookiesAmount();
-Seattle.totalCokieSoldFunc();
-Seattle.renderCookieAmmount();
+Shops.prototype.randCusHor = function(){
 
+  let randCustomers = getRndInteger(this.minCust,this.maxCust);
+  return randCustomers;
 
+};
 
+Shops.prototype.cookiesAmount = function(){
 
+  for (let i=0 ; i < 14 ; i++ ) {
+    let cookieAmountPerH = this.randCusHor() * this.avgCokie;
+    this.cookieSoldPerHour.push(Math.floor(cookieAmountPerH));
+  }
 
+};
 
-// This is Tokyo Object
+Shops.prototype.totalCokieSoldFunc = function(){
 
-let Tokyo ={
+  for(let i=0 ; i < this.cookieSoldPerHour.length ; i++){
 
-  shopLoc : 'Tokyo',
-  minCust : 3 ,
-  maxCust : 24 ,
-  avgCokie : 1.2 ,
-  cookieSoldPerHour :[],
-  totalCokiesSold : null,
+    this.totalCokiesSold = this.totalCokiesSold + this.cookieSoldPerHour[i];
 
-  randCusHor : function(){
-    let randCustomers = getRndInteger(this.minCust,this.maxCust)
-    return randCustomers;
-  },
-  cookiesAmount : function(){
-    for (let i=0 ; i < 14 ; i++ ) {
-      let cookieAmountPerH = this.randCusHor() * this.avgCokie;
-      this.cookieSoldPerHour.push(Math.floor(cookieAmountPerH));
-      console.log(this.cookieSoldPerHour);
-    }
+  }
 
-  },
+  this.cookieSoldPerHour.push(this.totalCokiesSold);
 
-  totalCokieSoldFunc : function() {
-    for(let i=0 ; i < this.cookieSoldPerHour.length ; i++){
-      this.totalCokiesSold = this.totalCokiesSold + this.cookieSoldPerHour[i];
-    }
-    this.cookieSoldPerHour.push(this.totalCokiesSold);
+};
 
-  },
-  renderCookieAmmount : function(){
+Shops.prototype.tableRender = function () {
 
-    let h2El = golbeVar.appendChild(document.createElement('h2'))
-    h2El.textContent = this.shopLoc;
+  let tr1 = document.createElement('tr');
+  tableEl.appendChild(tr1);
 
-    let ulEl = golbeVar.appendChild(document.createElement('ul'));
-    for ( let i=0 ; i<timeArr.length ; i++) {
+  for(let i = -1 ;i< timeArr.length ; i++){
 
-      let liEl = document.createElement('li');
-      ulEl.appendChild(liEl);
-      liEl.textContent =`${timeArr[i]} ${this.cookieSoldPerHour[i]}`;
-    }
-
-
-  },
-
+    let thEl = document.createElement('th');
+    tr1.appendChild(thEl);
+    thEl.textContent = timeArr[i];
+  }
 
 }
 
-Tokyo.cookiesAmount();
-Tokyo.totalCokieSoldFunc();
-Tokyo.renderCookieAmmount();
+Shops.prototype.render = function(){
+  
+  let tr1El = document.createElement('tr');
+  tableEl.appendChild(tr1El);
+  let td1 = document.createElement('td');
+  tr1El.appendChild(td1);
+  td1.textContent = this.shopLoc;
+
+  for(let j=0 ; j<this.cookieSoldPerHour.length;j++){
+
+    let td2 = document.createElement('td');
+    tr1El.appendChild(td2);
+    td2.textContent = this.cookieSoldPerHour[j];
 
 
+  }
+
+  arrOfObjects.push(this);
+  console.log(arrOfObjects);
+};
 
 
+let seattle = new Shops('Seattle',23,65,6.3);
+seattle.tableRender();
+seattle.cookiesAmount();
+seattle.totalCokieSoldFunc();
+seattle.render();
+let tokyo = new Shops('Tokyo',3,24,1.2);
+tokyo.cookiesAmount();
+tokyo.totalCokieSoldFunc();
+tokyo.render();
+let dubai = new Shops('Dubai',11,38,1.2);
+dubai.cookiesAmount();
+dubai.totalCokieSoldFunc();
+dubai.render();
+let paris = new Shops('Paris',20,38,2.3);
+paris.cookiesAmount();
+paris.totalCokieSoldFunc();
+paris.render();
+let lima = new Shops('Lima',2,16,4.6);
+lima.cookiesAmount();
+lima.totalCokieSoldFunc();
+lima.render();
 
-// This is Dubai Object
-let Dubai ={
-
-  shopLoc : 'Dubai',
-  minCust : 11 ,
-  maxCust : 38 ,
-  avgCokie : 3.7 ,
-  cookieSoldPerHour :[],
-  totalCokiesSold : null,
-
-  randCusHor : function(){
-    let randCustomers = getRndInteger(this.minCust,this.maxCust)
-    return randCustomers;
-  },
-  cookiesAmount : function(){
-    for (let i=0 ; i < 14 ; i++ ) {
-      let cookieAmountPerH = this.randCusHor() * this.avgCokie;
-      this.cookieSoldPerHour.push(Math.floor(cookieAmountPerH));
-      console.log(this.cookieSoldPerHour);
-    }
-
-  },
-
-  totalCokieSoldFunc : function() {
-    for(let i=0 ; i < this.cookieSoldPerHour.length ; i++){
-      this.totalCokiesSold = this.totalCokiesSold + this.cookieSoldPerHour[i];
-    }
-    this.cookieSoldPerHour.push(this.totalCokiesSold);
-
-  },
-  renderCookieAmmount : function(){
-
-    let h2El = golbeVar.appendChild(document.createElement('h2'))
-    h2El.textContent = this.shopLoc;
-
-    let ulEl = golbeVar.appendChild(document.createElement('ul'));
-    for ( let i=0 ; i<timeArr.length ; i++) {
-
-      let liEl = document.createElement('li');
-      ulEl.appendChild(liEl);
-      liEl.textContent =`${timeArr[i]} ${this.cookieSoldPerHour[i]}`;
-    }
-
-
-  },
-
-
-}
-
-Dubai.cookiesAmount();
-Dubai.totalCokieSoldFunc();
-Dubai.renderCookieAmmount();
-
-
-// This is Paris Object
-let Paris ={
-
-  shopLoc : 'Paris',
-  minCust : 20 ,
-  maxCust : 38 ,
-  avgCokie : 2.3 ,
-  cookieSoldPerHour :[],
-  totalCokiesSold : null,
-
-  randCusHor : function(){
-    let randCustomers = getRndInteger(this.minCust,this.maxCust)
-    return randCustomers;
-  },
-  cookiesAmount : function(){
-    for (let i=0 ; i < 14 ; i++ ) {
-      let cookieAmountPerH = this.randCusHor() * this.avgCokie;
-      this.cookieSoldPerHour.push(Math.floor(cookieAmountPerH));
-      console.log(this.cookieSoldPerHour);
-    }
-
-  },
-
-  totalCokieSoldFunc : function() {
-    for(let i=0 ; i < this.cookieSoldPerHour.length ; i++){
-      this.totalCokiesSold = this.totalCokiesSold + this.cookieSoldPerHour[i];
-    }
-    this.cookieSoldPerHour.push(this.totalCokiesSold);
-
-  },
-  renderCookieAmmount : function(){
-
-    let h2El = golbeVar.appendChild(document.createElement('h2'))
-    h2El.textContent = this.shopLoc;
-
-    let ulEl = golbeVar.appendChild(document.createElement('ul'));
-    for ( let i=0 ; i<timeArr.length ; i++) {
-
-      let liEl = document.createElement('li');
-      ulEl.appendChild(liEl);
-      liEl.textContent =`${timeArr[i]} ${this.cookieSoldPerHour[i]}`;
-    }
-
-
-  },
-
-
-}
-
-Paris.cookiesAmount();
-Paris.totalCokieSoldFunc();
-Paris.renderCookieAmmount();
-
-
-
-
-// This is Lima Object
-let Lima ={
-
-  shopLoc : 'Lima',
-  minCust : 20 ,
-  maxCust : 38 ,
-  avgCokie : 2.3 ,
-  cookieSoldPerHour :[],
-  totalCokiesSold : null,
-
-  randCusHor : function(){
-    let randCustomers = getRndInteger(this.minCust,this.maxCust)
-    return randCustomers;
-  },
-  cookiesAmount : function(){
-    for (let i=0 ; i < 14 ; i++ ) {
-      let cookieAmountPerH = this.randCusHor() * this.avgCokie;
-      this.cookieSoldPerHour.push(Math.floor(cookieAmountPerH));
-      console.log(this.cookieSoldPerHour);
-    }
-
-  },
-
-  totalCokieSoldFunc : function() {
-    for(let i=0 ; i < this.cookieSoldPerHour.length ; i++){
-      this.totalCokiesSold = this.totalCokiesSold + this.cookieSoldPerHour[i];
-    }
-    this.cookieSoldPerHour.push(this.totalCokiesSold);
-
-  },
-  renderCookieAmmount : function(){
-
-    let h2El = golbeVar.appendChild(document.createElement('h2'))
-    h2El.textContent = this.shopLoc;
-
-    let ulEl = golbeVar.appendChild(document.createElement('ul'));
-    for ( let i=0 ; i<timeArr.length ; i++) {
-
-      let liEl = document.createElement('li');
-      ulEl.appendChild(liEl);
-      liEl.textContent =`${timeArr[i]} ${this.cookieSoldPerHour[i]}`;
-    }
-
-
-  },
-
-
-}
-
-Lima.cookiesAmount();
-Lima.totalCokieSoldFunc();
-Lima.renderCookieAmmount();
+console.log(firstTotalArr);
